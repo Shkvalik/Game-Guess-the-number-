c5824aa93099ca122c2ff6d4406dacaf8cad8dac
Shkvalik <81921559+Shkvalik@users.noreply.github.com>
2021-07-08 15:34:00 +0300

    IMLEMENT A LEVEL SYSTEM - DONE! IMPLEMENT A SAVE SYSTEM - STARTED!

--- JavaTraining2/src/com/geekbrains/lesson3/Main.java
+++ JavaTraining2/src/com/geekbrains/lesson3/Main.java
@@ -7,31 +7,38 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi! I am game created by the Shkvalik." +
                 "\nLet's play the game! I will guess the number for you and you will have to guess it." +
-                "\nChoose difficulty:\n1.Easy\n2.Medium\n3.Hard"); //displaying a greeting
+                "\nFor the first Say me your name:");//displaying a greeting
+        String name = scan.next();
         int[] d_conditions = {0, 0}; //variable for a range of random numbers
         int i = 0; //variable for counting attempts
+        System.out.println("Good!\nNext you must choose the difficult:\n1.Easy\n2.Medium\n3.Hard");
         d_conditions = ChooseLevel(d_conditions);
-        System.out.println("Nice! Now guess from 0 to " + d_conditions[0] + "\nYou have " + d_conditions[1] + " attempt");//Say range of random number and have left attempts
-        int number = (int) (Math.random() * d_conditions[0]); //system expression for specifying the range of the hidden number
-        PlayLevel(i, number, d_conditions);
+        for (int cur_number = 5; cur_number <= d_conditions[0]; cur_number += 5){PlayLevel(i, cur_number, d_conditions);}
+        System.out.println("You win! Game is passed");
     }
-    private static void PlayLevel(int i, int number, int[]d_conditions){
-            while (i < d_conditions[1]) { //they say that the loop must work as long as the attempts used are less than the given by the difficulty
-                int input_number = scan.nextInt();//remember a number
+
+    private static void PlayLevel(int i, int cur_number, int[]d_conditions){
+        System.out.println("Nice! Now guess from 0 to " + cur_number + "\nYou have " + d_conditions[1] + " attempt");//Say range of random number and have left attempts
+        int number = (int) (Math.random() * cur_number);//system expression for specifying the range of the hidden number
+        System.out.println(number); //DELETE LATER!
+        int input_number = -1;
+        while(number != input_number) { //they say that the loop must work as long as the attempts used are less than the given by the difficulty
+            input_number = scan.nextInt();
                 if (input_number == number) { //conditions if user has written the correct number that
                     System.out.println("Awesome!You guessed right!");
                 } else if (input_number > number) { //conditions if user has written the bigger number than conceived number
                     System.out.println("Your answer is bigger than conceived number");
                     i++;//increase the value by one
+                    System.out.println("You have left " + (d_conditions[1] - i) + " attempts");//the remaining attempts are displayed
                 } else { //conditions if user has written the smaller number than conceived number
                     System.out.println("Your answer is smaller than conceived number");
                     i++;//increase the value by one
+                    System.out.println("You have left " + (d_conditions[1] - i) + " attempts");//the remaining attempts are displayed
                 }
                 if(i == d_conditions[1]){//condition if attempts have no more
                     System.out.println("You Lose!\nThe hidden number - " + number);
                     System.exit(0);
                 }
-                System.out.println("You have left " + (d_conditions[1] - i) + " attempts");//the remaining attempts are displayed
             }
     }
     private static int[] ChooseLevel(int[] d_conditions){
@@ -51,7 +58,7 @@ public class Main {
 }
 /* TODO: 05.07.2021
 Comment all metod ChooseLevel and Return he in main metod //Я МАТЬ ЕГО ЭТО СДЕЛАЛ!!!!! by Shkvalik
-!implement a level system with help cycle FOR!
+!implement a level system with help cycle FOR!//АХАХХАХ ТА Я МАТЬ ЕГО БОГ КОДИНГА!!!!by Shkvalik
 implement a help system
 implement a score system
 implement a save score system(work with file)
